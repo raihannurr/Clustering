12547c2b277e98da68b761d038a913df7466ba1b
mhartopo <udaraapi@rocketmail.com>
2016-11-22 12:52:07 +0700

    cgange node strucuture

--- src/main/Node.java
+++ src/main/Node.java
@@ -1,21 +1,24 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Node <T> {
-	private T element;
+	private ArrayList<T> element;
 	private boolean isLeaf;
 	
 	public Node() {
 		this.element = null;
 	}
-	public Node(T element) {
+	
+	public Node(ArrayList<T> element) {
 		this.setElement(element);
 	}
 
-	public T getElement() {
+	public ArrayList<T> getElement() {
 		return element;
 	}
 
-	public void setElement(T element) {
+	public void setElement(ArrayList<T> element) {
 		this.element = element;
 	}
 
